4e1204b4c8f4ab81dc32dfb55fd1b7ff4b3ea2f1
Jonathan Palardy <jonathan.palardy@gmail.com>
2013-06-01 16:28:49 -0400

    burn kanji2012 images

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
+
 ssh_options[:compression] = false
 
 set  :application,    "home"
 
 set  :use_sudo,       false
-role :app,            "jonathan@jpalardy.com"
+role :app,            "atlas@jpalardy.com"
 
 set :repository,      '.'
 set :scm,             :git
 set :deploy_via,      :copy
-set :deploy_to,       "/home/jonathan/production/#{application}"
+set :deploy_to,       "/home/atlas/#{application}"
 set :copy_strategy,   :export
 
 # don't do a bunch of Rails stuff
@@ -17,12 +18,5 @@ namespace :deploy do
   task :stop do ; end
   task :restart do ; end
   task :finalize_update do ; end
-end
-
-after "deploy:update_code", "deploy:grab_last_img"
-namespace :deploy do
-  task :grab_last_img do
-    run "cp #{previous_release}/public/kanji2012/img/*.png #{release_path}/public/kanji2012/img/"
-  end
 end
 
