800a9d64ecb07f6356e1bc7c2ec34270db64f1f1
Jonathan Palardy <jonathan.palardy@gmail.com>
2014-08-29 20:35:44 -0700

    refactor to assert_sites

--- test/command.js
+++ test/command.js
@@ -14,6 +14,11 @@ Command.sites = (function () {
 }());
 Command.default_sites = ['ddg'];
 
+var assert_sites = function (query, sites) {
+  var command = Command.parse(query);
+  assert.deepEqual(command && command.urls, sites); // command && to test null
+};
+
 describe('Command', function () {
 
   describe('constructor', function () {
@@ -30,65 +35,56 @@ describe('Command', function () {
 
   describe('parse', function () {
     it('punts an empty query', function () {
-      var command = Command.parse('');
-      assert.strictEqual(command, null);
+      assert_sites('', null);
     });
 
     it('punts a blank query', function () {
-      var command = Command.parse('     ');
-      assert.strictEqual(command, null);
+      assert_sites('     ', null);
     });
 
     it('handles a one-word query', function () {
-      var command = Command.parse('gim something');
-      assert.deepEqual(command.urls, ['https://www.google.com/search?q=something&tbm=isch']);
+      assert_sites('gim something', ['https://www.google.com/search?q=something&tbm=isch']);
     });
 
     it('handles a two-word query', function () {
-      var command = Command.parse('gim some thing');
-      assert.deepEqual(command.urls, ['https://www.google.com/search?q=some%20thing&tbm=isch']);
+      assert_sites('gim some thing', ['https://www.google.com/search?q=some%20thing&tbm=isch']);
     });
 
     it('handles a one-word query (default)', function () {
-      var command = Command.parse('something');
-      assert.deepEqual(command.urls, ['https://duckduckgo.com/?q=something']);
+      assert_sites('something', ['https://duckduckgo.com/?q=something']);
     });
 
     it('handles a two-word query (default)', function () {
-      var command = Command.parse('some thing');
-      assert.deepEqual(command.urls, ['https://duckduckgo.com/?q=some%20thing']);
+      assert_sites('some thing', ['https://duckduckgo.com/?q=some%20thing']);
     });
 
     it('handles a site without a query', function () {
-      var command = Command.parse('g');
-      assert.deepEqual(command.urls, ['https://www.google.com/']);
+      assert_sites('g', ['https://www.google.com/']);
     });
 
     it('handles two sites without a query', function () {
-      var command = Command.parse('g,yim');
-      assert.deepEqual(command.urls, ['https://www.google.com/', 'http://images.search.yahoo.com/search/images']);
+      assert_sites('g,yim', ['https://www.google.com/',
+                             'http://images.search.yahoo.com/search/images']);
     });
 
     it('handles two sites', function () {
-      var command = Command.parse('gim,yim something');
-      assert.deepEqual(command.urls, ['https://www.google.com/search?q=something&tbm=isch', 'http://images.search.yahoo.com/search/images?p=something']);
+      assert_sites('gim,yim something', ['https://www.google.com/search?q=something&tbm=isch',
+                                         'http://images.search.yahoo.com/search/images?p=something']);
     });
 
     it('ignores unknown sites', function () {
-      var command = Command.parse('gim,unknown,yim,blah something');
-      assert.deepEqual(command.urls, ['https://www.google.com/search?q=something&tbm=isch', 'http://images.search.yahoo.com/search/images?p=something']);
+      assert_sites('gim,unknown,yim,blah something', ['https://www.google.com/search?q=something&tbm=isch',
+                                                      'http://images.search.yahoo.com/search/images?p=something']);
     });
   });
 
   describe('parse (legacy)', function () {
     it('does not handle site as a query', function () {
-      var command = Command.parse('gim yim');
-      assert.deepEqual(command.urls, ['https://www.google.com/search?q=yim&tbm=isch']);
+      assert_sites('gim yim', ['https://www.google.com/search?q=yim&tbm=isch']);
     });
 
     it('does not handle site as a query, with a query', function () {
-      var command = Command.parse('gim yim something');
-      assert.deepEqual(command.urls, ['https://www.google.com/search?q=yim%20something&tbm=isch']);
+      assert_sites('gim yim something', ['https://www.google.com/search?q=yim%20something&tbm=isch']);
     });
   });
 });
