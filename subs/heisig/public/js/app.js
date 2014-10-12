243b9fe37e03a390874a62db36862201cd3df28f
Jonathan Palardy <jonathan.palardy@gmail.com>
2014-10-12 01:37:11 -0400

    new heisig on react.js

--- subs/heisig/public/js/app.js
+++ subs/heisig/public/js/app.js
@@ -1,177 +1,89 @@
-/* global Backbone, $ */
+/* global $, React, starific */
 
-var app = {};
-
-(function (exports) {
+(function () {
   'use strict';
 
-  $(function () {
-    var deck = exports.deck = new exports.Deck();
-    exports.deck_view   = new exports.DeckView({model: deck, el: $('#cards')});
-    exports.search_view = new exports.SearchView({model: deck, el: $('#search')});
-    exports.workspace   = new exports.Workspace({deck: deck});
-    Backbone.history.start();
-    deck.fetch();
-
-    $(document).keypress(function (e) {
-      if (e.which === 47 || e.which === 104) { // / or h
-        e.preventDefault();
-        $('#search input').select().focus();
-      }
-    });
-  });
-}(app));
-/* global Backbone, app, _, starific */
-
-(function (exports) {
-  'use strict';
-
-  var Card = Backbone.Model.extend({
-    initialize: function () {
-      this.set('tokens', _.flatten([
-        this.tokenize(this.get('keyword')),
-        this.get('no').toString(),
-        this.get('kanji')
-      ]));
+  var DeckFilter = React.createClass({
+    getInitialState: function () {
+      return {query: this.props.query};
     },
-    tokenize: function (str) {
+    setQuery: function (query) {
+      location.hash = encodeURIComponent(query).replace(/%20/g, '+');
+      this.setState({query: query});
+    },
+    render: function () {
+      return React.DOM.div({},
+        new SearchBar({query: this.state.query, setQuery: this.setQuery}),
+        new Deck({cards: this.props.cards, query: this.state.query})
+      );
+    }
+  });
+
+  var SearchBar = React.createClass({
+    handleSubmit: function (e) {
+      e.preventDefault();
+    },
+    handleChange: function () {
+      var query = this.refs.query.getDOMNode().value;
+      this.props.setQuery(query);
+    },
+    render: function () {
+      return React.DOM.div({id: 'content'},
+        React.DOM.form({id: 'search', onSubmit: this.handleSubmit},
+          React.DOM.input({type: 'search', ref: 'query', size: 35, autoFocus: 'autofocus', autoCapitalize: 'off', autoCorrect: 'off', results: 'results', value: this.props.query, onChange: this.handleChange})
+        )
+      );
+    }
+  });
+
+  var Deck = React.createClass({
+    render: function () {
+      var query    = this.props.query;
+      var matchers = starific(query);
+      var cards    = this.props.cards;
+      if (query) {
+        cards = cards.filter(function (card) {
+          return matchers.any(card.tokens);
+        });
+      }
+      var count = "no kanjis";
+      if (cards.length === 1) { count = "1 kanji"; }
+      if (cards.length > 1)   { count = cards.length + " kanjis"; }
+      return React.DOM.div({},
+        React.DOM.span({id: 'count'}, count),
+        React.DOM.div({className: 'cards'}, cards.map(Card))
+      );
+    },
+  });
+
+  var Card = React.createClass({
+    render: function () {
+      return React.DOM.div({className: 'card'},
+        React.DOM.span({className: 'no'}, this.props.no),
+        React.DOM.span({className: 'keyword'}, this.props.keyword),
+        React.DOM.span({className: 'kanji'}, this.props.kanji)
+      );
+    }
+  });
+
+  $.getJSON('heisig.min.json', function (cards) {
+    var tokenize = function (str) {
       // "muffin's" -> "muffin's muffin"
       var possessiveWord = function (word) {
         return [word, word.replace(/'s\b/, '')].join(' ');
       };
-      return str.toLowerCase().replace(/-/g, ' ').
-                               replace(/\w+'s\b/, possessiveWord).
-                               replace(/[^a-z' ]/g, '').
-                               trim().split(/\s+/);
-    }
+      return str.toLowerCase().replace(/-/g, ' ').replace(/\w+'s\b/, possessiveWord).replace(/[^a-z' ]/g, '').trim().split(/\s+/);
+    };
+    cards = cards.map(function (card, i) {
+      var result = {};
+      result.no      = String(i + 1);
+      result.key     = 'card-' + result.no;
+      result.kanji   = Object.keys(card)[0];
+      result.keyword = card[result.kanji];
+      result.tokens  = tokenize(result.keyword).concat([result.no, result.kanji]);
+      return result;
+    });
+    var query = decodeURIComponent(location.hash.replace(/^#/, '').replace(/\+/g, "%20"));
+    React.renderComponent(new DeckFilter({cards: cards, query: query}), document.getElementById('dump'));
   });
-
-  var Cards = Backbone.Collection.extend({
-    model: Card
-  });
-
-  exports.Deck = Backbone.Model.extend({
-    url: 'heisig.json',
-    initialize: function () {
-      this.set('cards', new Cards());
-      this.on('change:filter', function (deck, query) {
-        Backbone.history.navigate(query);
-      });
-    },
-    parse: function (resp) {
-      return {cards: new Cards(resp)};
-    },
-    filtered: function () {
-      var query = this.get('filter');
-      if (!query) {
-        return this.get('cards');
-      }
-      var matchers = starific(query);
-      return this.get('cards').filter(function (card) {
-        return matchers.any(card.get('tokens'));
-      });
-    }
-  });
-}(app));
-/* global Backbone, $, app, Mustache */
-
-(function (exports) {
-  'use strict';
-
-  var CACHE = [];
-
-  var pluralize = function (count, singular, plural) {
-    plural = plural || singular + 's';
-    return count + ' ' + (count === 1 ? singular : plural);
-  };
-
-  var highlight = function (sel, duration) {
-    sel = $(sel);
-    sel.addClass('highlighted');
-    setTimeout(function () {
-      sel.removeClass('highlighted');
-    }, duration);
-  };
-
-  //-------------------------------------------------
-
-  var CardView = Backbone.View.extend({
-    className: 'card',
-    template: $('#card-template').html(),
-    render: function () {
-      this.$el.html(Mustache.render(this.template, this.model.toJSON()));
-      return this;
-    }
-  });
-
-  exports.DeckView = Backbone.View.extend({
-    events: {
-      'click a': 'select'
-    },
-    select: function (e) {
-      e.preventDefault();
-      var kno = $(e.target).text();
-      $('#search input').val('');
-      this.model.set('filter', '');
-      var target = $("#card-" + kno);
-      highlight(target, 2000);
-      $(window).scrollTop(target.offset().top - 130);
-    },
-    initialize: function () {
-      this.model.on('change', this.render, this);
-    },
-    render: function () {
-      var cards = this.model.filtered();
-      $('#count').html(pluralize(cards.length, 'kanji'));
-      this.$el.html(cards.map(function (card) {
-        var kno = card.get('no');
-        if (!CACHE[kno]) {
-          var cardView = new CardView({model: card, id: "card-" + card.get('no')});
-          CACHE[kno] = cardView.render().el;
-        }
-        return CACHE[kno];
-      }));
-      return this;
-    }
-  });
-
-  exports.SearchView = Backbone.View.extend({
-    events: {
-      'change':   'change',
-      'click':    'change',
-      'keypress': 'press'
-    },
-    change: function (e) {
-      this.filter($(e.target).val());
-    },
-    press: function (e) {
-      e.stopPropagation();
-      if (e.which === 13) { // return
-        e.preventDefault();
-        this.change(e);
-      }
-    },
-    filter: function (query) {
-      return this.model.set('filter', query);
-    }
-  });
-}(app));
-/* global Backbone, $, app */
-
-(function (exports) {
-  'use strict';
-
-  exports.Workspace = Backbone.Router.extend({
-    routes: {
-      ":query": "search"
-    },
-    initialize: function (options) {
-      this.deck = options.deck;
-    },
-    search: function (q) {
-      q = decodeURIComponent(q.replace(/\+/g, "%20"));
-      $('#search input').val(q);
-      this.deck.set('filter', q);
-    }
-  });
-}(app));
+}());
