77ff821f2ab981b396e8706eb57de0f6fa008b73
Jonathan Palardy <jonathan.palardy@gmail.com>
2014-10-30 17:17:06 -0700

    heisig: React.DOM --> React.createElement

--- subs/heisig/src/js/components.js
+++ subs/heisig/src/js/components.js
@@ -8,10 +8,10 @@ var starific = require('./starific');
 
 var Card = exports.Card = React.createClass({
   render: function () {
-    return React.DOM.div({className: 'card'},
-      React.DOM.span({className: 'no'}, this.props.no),
-      React.DOM.span({className: 'keyword'}, this.props.keyword),
-      React.DOM.span({className: 'kanji'}, this.props.kanji)
+    return React.createElement('div', {className: 'card'},
+      React.createElement('span', {className: 'no'}, this.props.no),
+      React.createElement('span', {className: 'keyword'}, this.props.keyword),
+      React.createElement('span', {className: 'kanji'}, this.props.kanji)
     );
   }
 });
@@ -25,9 +25,9 @@ var SearchBar = exports.SearchBar = React.createClass({
     this.props.setQuery(query);
   },
   render: function () {
-    return React.DOM.div({id: 'content'},
-      React.DOM.form({id: 'search', onSubmit: this.handleSubmit},
-        React.DOM.input({type: 'search', ref: 'query', size: 35, autoFocus: 'autofocus', autoCapitalize: 'off', autoCorrect: 'off', results: 'results', value: this.props.query, onChange: this.handleChange})
+    return React.createElement('div', {id: 'content'},
+      React.createElement('form', {id: 'search', onSubmit: this.handleSubmit},
+        React.createElement('input', {type: 'search', ref: 'query', size: 35, autoFocus: 'autofocus', autoCapitalize: 'off', autoCorrect: 'off', results: 'results', value: this.props.query, onChange: this.handleChange})
       )
     );
   }
@@ -46,9 +46,9 @@ var Deck = exports.Deck = React.createClass({
     var count = "no kanjis";
     if (cards.length === 1) { count = "1 kanji"; }
     if (cards.length > 1)   { count = cards.length + " kanjis"; }
-    return React.DOM.div({},
-      React.DOM.span({id: 'count'}, count),
-      React.DOM.div({className: 'cards'}, cards.map(Card))
+    return React.createElement('div', {},
+      React.createElement('span', {id: 'count'}, count),
+      React.createElement('div', {className: 'cards'}, cards.map(function (card) { return React.createElement(Card, card); }))
     );
   },
 });
@@ -63,7 +63,7 @@ exports.DeckFilter = React.createClass({
     this.setState({query: query});
   },
   render: function () {
-    return React.DOM.div({},
+    return React.createElement('div', {},
       React.createElement(SearchBar, {query: this.state.query, setQuery: this.setQuery}),
       React.createElement(Deck, {cards: this.props.cards, query: this.state.query})
     );
