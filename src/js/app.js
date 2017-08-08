b2d72331e95ea93d800e13f29a2adab529719dcb
Jonathan Palardy <jonathan.palardy@gmail.com>
2017-08-08 15:17:25 -0700

    upgrade code based on learnings

--- src/js/app.js
+++ src/js/app.js
@@ -3,124 +3,115 @@
 const sites   = require('./sites');
 const Command = require('./command')(sites, 'g');
 
+// import CSS for webpack
 require('../less/main.less');
 
-const get = function (sel) { return document.getElementById(sel); };
-
-// maintains completion state
-let circularCompletions = [];
+const get = document.getElementById.bind(document);
 
 //-------------------------------------------------
 // actions
 //-------------------------------------------------
 
-const setCommand = function (text) {
-  if (text === undefined) { return; }
-  get('command_input').value = text;
-};
+const ACTIONS = {
+  setCommand(text) {
+    if (text === undefined) { return; }
+    get('command_input').value = text;
+  },
 
-const getText = function () {
-  return get('command_input').value.trim();
-};
+  getText() {
+    return get('command_input').value.trim();
+  },
 
-const getCommand = function () {
-  return Command.parse(getText());
-};
-
-const submit = function () {
-  const command = getCommand();
-  if (!command) {
-    return false;
-  }
-  window.location = command.url;
-  return true;
-};
-
-const toggleCheatSheet = function () {
-  const elem = get('cheatSheetDetails');
-  elem.className = (elem.className === 'hide' ? '' : 'hide');
-};
-
-const reduceCheatSheet = function (text) {
-  let lines = Command.cheatSheet.filter(line => line.indexOf(text) === 0);
-  if (!lines.length) {
-    lines = Command.cheatSheet;
-  }
-  get('cheatSheet').innerHTML = lines.join('\n');
-};
-
-const complete = function (text) {
-  if (!text) { return; }
-  const newText = (function () {
-    if (circularCompletions.length > 0) {
-      const i = circularCompletions.indexOf(text);
-      return circularCompletions[(i + 1) % circularCompletions.length];
+  submit() {
+    const command = Command.parse(this.getText());
+    if (!command) {
+      return;
     }
-    const completions = sites
-      .filter(site => site.alias.indexOf(text) === 0)
-      .map(site => site.alias);
-    if (completions.length === 0) { return text; }  // no match, return original
-    circularCompletions = completions.concat(text); // save completions and original text
-    return completions[0];
-  }());
-  setCommand(newText);
+    //console.log("*** window.location =", command.url)
+    window.location = command.url;
+  },
+
+  toggleCheatSheet() {
+    get('cheatSheetDetails').classList.toggle('hide');
+  },
+
+  reduceCheatSheet(text) {
+    get('cheatSheet').innerHTML = Command.cheatSheet(text).join('\n');
+  },
 };
 
 //-------------------------------------------------
 // deal with q= param
 //-------------------------------------------------
 
-const getParams = function (query) {
-  const result = {};
-  query = query || document.location.search.substring(1);
-  query.split('&').forEach((param) => {
-    const parts = param.split('=', 2);
-    result[parts[0]] = decodeURIComponent(parts[1]).replace(/\+/g, ' ');
-  });
-  return result;
-};
+{
+  const getParams = function (query) {
+    const result = {};
+    query = query || document.location.search.substring(1);
+    query.split('&').forEach((param) => {
+      const parts = param.split('=', 2);
+      result[parts[0]] = decodeURIComponent(parts[1]).replace(/\+/g, ' ');
+    });
+    return result;
+  };
 
-setCommand(getParams().q);
-submit();
+  ACTIONS.setCommand(getParams().q);
+  ACTIONS.submit();
+}
 
 //-------------------------------------------------
 // event handlers
 //-------------------------------------------------
 
-get('command_form').onsubmit = function () {
-  submit();
-  return false;
-};
-
-document.body.onkeyup = function (ev) {
-  if (ev.keyCode === 27) { // ESC
-    toggleCheatSheet();
-    return false;
-  }
-  if (ev.keyCode === 191) { // forward slash '/'
+{
+  document.body.addEventListener('keyup', (ev) => {
+    if (ev.keyCode === 27) { // ESC
+      ACTIONS.toggleCheatSheet();
+      return;
+    }
+    // any key focused search field
     get('command_input').focus();
-    return false;
-  }
-  return true;
-};
+  }, true); // capture event on way down
 
-get('command_input').onkeydown = function (ev) {
-  if (ev.keyCode === 27) { // ESC
-    ev.preventDefault();   // don't clear the text field
-    return false;
-  }
-  if (ev.keyCode === 9) {  // TAB
+  //-------------------------------------------------
+  // maintains completion state
+  let circularCompletions = [];
+  const complete = function (text) {
+    if (!text) { return; }
+    const newText = (function () {
+      if (circularCompletions.length > 0) {
+        const i = circularCompletions.indexOf(text);
+        return circularCompletions[(i + 1) % circularCompletions.length];
+      }
+      const completions = sites
+        .filter(site => site.alias.indexOf(text) === 0)
+        .map(site => site.alias);
+      if (completions.length === 0) { return text; }  // no match, return original
+      circularCompletions = completions.concat(text); // save completions and original text
+      return completions[0];
+    }());
+    ACTIONS.setCommand(newText);
+  };
+  //-------------------------------------------------
+
+  const commandForm = get('command_form');
+
+  commandForm.addEventListener('submit', (ev) => {
     ev.preventDefault();
-    complete(getText());
-    return false;
-  }
-  return true;
-};
+    ACTIONS.submit();
+  });
 
-get('command_input').onkeyup = function (ev) {
-  if (ev.keyCode !== 9) { // TAB
-    circularCompletions = []; // reset completions, some other key was pressed
-  }
-  reduceCheatSheet(getText().split(/\s+/)[0]);
-  return true;
-};
+  commandForm.addEventListener('keydown', (ev) => {
+    if (ev.keyCode === 9) {  // TAB
+      ev.preventDefault();
+      complete(ACTIONS.getText());
+    }
+  });
+
+  commandForm.addEventListener('keyup', (ev) => {
+    if (ev.keyCode !== 9) { // TAB
+      circularCompletions = []; // reset completions, some other key was pressed
+    }
+    ACTIONS.reduceCheatSheet(ACTIONS.getText().split(/\s+/)[0]);
+  });
+}
--- src/js/command.js
+++ src/js/command.js
@@ -1,52 +1,60 @@
 
-const Command = function (site, query, url) {
-  this.site = site;
-  this.query = query;
-  this.url = url;
-};
+class Command {
+  constructor(site, query, url) {
+    this.site  = site;
+    this.query = query;
+    this.url   = url;
+  }
 
-Command.prototype.toString = function () {
-  return [this.site.alias, this.query].join(' ');
-};
+  toString() {
+    return `${this.site.alias} ${this.query}`;
+  }
+}
 
 //-------------------------------------------------
 
 module.exports = function (sites, defaultSiteName) {
-  let cheatSheet; // set after augmentation below
-  sites = (function () {
-    const result = {};
-    sites.forEach((site) => {
-      site.visit = site.visit || site.search.match('^https?://[^/]+/')[0];
-      site.alias = site.alias || site.name;
-      result[site.alias] = site;
-    });
-    cheatSheet = sites.filter(site => !site.hide).map(site => `${site.alias}\t${site.name}`);
-    return result;
-  }());
+  // augment sites
+  sites.forEach((site) => {
+    site.visit = site.visit || site.search.match('^https?://[^/]+/')[0];
+    site.alias = site.alias || site.name;
+  });
+  const cheatSheet = sites
+    .filter(site => !site.hide)
+    .map(site => `${site.alias}\t${site.name}`);
+  const LUT = sites.reduce((acc, site) => {
+    acc[site.alias] = site;
+    return acc;
+  }, {});
 
-  const parse = function (text) {
-    const words = text.trim().split(/ +/).filter(Boolean);
-    // text is blank
-    if (words.length === 0) {
-      return null;
-    }
-    // first word supposed to be an existing site
-    // if not, parse again with default site
-    const site = sites[words[0]];
-    if (!site) {
-      return parse(`${defaultSiteName} ${text}`);
-    }
-    // query is all remaining words
-    const query = words.slice(1).join(' ');
-    // empty query means 'visit'
-    // otherwise search
-    let url = site.visit;
-    if (query) {
+  return {
+    cheatSheet(text) {
+      const lines = cheatSheet.filter(line => line.indexOf(text) === 0);
+      // match:   filtered down list
+      // nomatch: _everything_
+      return lines.length ? lines : cheatSheet;
+    },
+    parse(text) {
+      const words = text.trim().split(/ +/).filter(Boolean);
+      // text is blank
+      if (words.length === 0) {
+        return null;
+      }
+      // first word supposed to be an existing site
+      // query is all remaining words
+      const [first, ...rest] = words;
+      const site = LUT[first];
+      const query = rest.join(' ');
+      // if not, parse again with default site
+      if (!site) {
+        return this.parse(`${defaultSiteName} ${text}`);
+      }
+      // empty query means 'visit', otherwise 'search'
+      if (!query) {
+        return new Command(site, '', site.visit);
+      }
       const encodedQuery = encodeURIComponent(query).replace(/%20/g, '+');
-      url = site.search.replace(/%s/g, encodedQuery);
-    }
-    return new Command(site, query, url);
+      return new Command(site, query, site.search.replace(/%s/g, encodedQuery));
+    },
   };
-
-  return {parse, cheatSheet};
 };
