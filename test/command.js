a287855c6004fdfda3289669c08c5c11e437d7af
Jonathan Palardy <jonathan.palardy@gmail.com>
2018-04-08 16:39:50 -0700

    Make prettier/eslint happy

--- test/command.js
+++ test/command.js
@@ -1,57 +1,57 @@
 /* global describe, it */
 
-const assert = require('assert');
+const assert = require("assert");
 
-const sites   = require('../src/js/websites');
-const Command = require('../src/js/command')(sites, 'ddg');
+const sites = require("../src/js/websites");
+const Command = require("../src/js/command")(sites, "ddg");
 
-const assertURL = function (text, url) {
+const assertURL = function(text, url) {
   const command = Command.parse(text);
   assert.strictEqual(command && command.url, url); // command && to test null
 };
 
-describe('Command', () => {
-  describe('parse', () => {
-    it('punts an empty query', () => {
-      assertURL('', null);
+describe("Command", () => {
+  describe("parse", () => {
+    it("punts an empty query", () => {
+      assertURL("", null);
     });
 
-    it('punts a blank query', () => {
-      assertURL('     ', null);
+    it("punts a blank query", () => {
+      assertURL("     ", null);
     });
 
-    it('handles a one-word query', () => {
-      assertURL('gim something', 'https://www.google.com/search?q=something&tbm=isch');
+    it("handles a one-word query", () => {
+      assertURL("gim something", "https://www.google.com/search?q=something&tbm=isch");
     });
 
-    it('handles a two-word query', () => {
-      assertURL('gim some thing', 'https://www.google.com/search?q=some+thing&tbm=isch');
+    it("handles a two-word query", () => {
+      assertURL("gim some thing", "https://www.google.com/search?q=some+thing&tbm=isch");
     });
 
-    it('handles a one-word query (default)', () => {
-      assertURL('something', 'https://duckduckgo.com/?q=something');
+    it("handles a one-word query (default)", () => {
+      assertURL("something", "https://duckduckgo.com/?q=something");
     });
 
-    it('handles a two-word query (default)', () => {
-      assertURL('some thing', 'https://duckduckgo.com/?q=some+thing');
+    it("handles a two-word query (default)", () => {
+      assertURL("some thing", "https://duckduckgo.com/?q=some+thing");
     });
 
-    it('handles a site without a query', () => {
-      assertURL('g', 'https://www.google.com/');
+    it("handles a site without a query", () => {
+      assertURL("g", "https://www.google.com/");
     });
 
-    it('handles worst case with bad spacing', () => {
-      assertURL('    gim some     thing      ', 'https://www.google.com/search?q=some+thing&tbm=isch');
+    it("handles worst case with bad spacing", () => {
+      assertURL("    gim some     thing      ", "https://www.google.com/search?q=some+thing&tbm=isch");
     });
   });
 
-  describe('parse (legacy)', () => {
-    it('does not handle site as a query', () => {
-      assertURL('gim yim', 'https://www.google.com/search?q=yim&tbm=isch');
+  describe("parse (legacy)", () => {
+    it("does not handle site as a query", () => {
+      assertURL("gim yim", "https://www.google.com/search?q=yim&tbm=isch");
     });
 
-    it('does not handle site as a query, with a query', () => {
-      assertURL('gim yim something', 'https://www.google.com/search?q=yim+something&tbm=isch');
+    it("does not handle site as a query, with a query", () => {
+      assertURL("gim yim something", "https://www.google.com/search?q=yim+something&tbm=isch");
     });
   });
 });
--- test/completer.js
+++ test/completer.js
@@ -1,35 +1,45 @@
 /* global describe, it, before */
-/* eslint prefer-arrow-callback: 0 */
+/* eslint */
 
-const assert = require('assert');
+const assert = require("assert");
 
-const Completer = require('../src/js/completer');
+const Completer = require("../src/js/completer");
 
-describe('Completer', function () {
-  describe('class', function () {
-    before(function () {
+describe("Completer", () => {
+  describe("class", () => {
+    before(function _before() {
       // all aliases as of 2018-02-10
-      this.words = 'g gt gim gmap w3w gfin tw yt b sh wa ddg wp mw ud emoji so cv am am.ca audible vpl bp leanpub ebooks-it libgen imdb yelp rt gh dh ls ch caniuse ann anidb cr fun nf heisig rd wr cnm fs api.nodejs api.express api.jq api.aws api.mdn meetup udemy'.split(' ');
+      this.words = `
+        g gt gim gmap w3w gfin tw yt
+        b sh wa ddg wp mw ud emoji
+        so cv am am.ca audible vpl bp leanpub
+        ebooks-it libgen imdb yelp rt gh dh ls
+        ch caniuse ann anidb cr fun nf heisig
+        rd wr cnm fs api.nodejs api.express api.jq api.aws
+        api.mdn meetup udemy
+      `
+        .trim()
+        .split(/[ \n]+/);
       this.subject = new Completer(this.words);
     });
 
-    it('contains words as given', function () {
+    it("contains words as given", function _test() {
       assert.deepEqual(this.subject.words, this.words);
     });
 
-    it('starts without completions', function () {
+    it("starts without completions", function _test() {
       assert.deepEqual(this.subject.completions, []);
     });
 
-    it('cycles through completions', function () {
+    it.only("cycles through completions", function _test() {
       {
-        const matches = this.subject.matches('c');
-        const words = ['c', 'cv', 'ch', 'caniuse', 'cr', 'cnm', 'c', 'cv', 'ch'/* ... */];
+        const matches = this.subject.matches("c");
+        const words = ["c", "cv", "ch", "caniuse", "cr", "cnm", "c", "cv", "ch"]; // keeps going...
         words.forEach(word => assert.strictEqual(matches.next().value, word));
       }
       {
-        const matches = this.subject.matches('am');
-        const words = ['am', 'am.ca', 'am', 'am.ca', 'am'];
+        const matches = this.subject.matches("am");
+        const words = ["am", "am.ca", "am", "am.ca", "am"];
         words.forEach(word => assert.strictEqual(matches.next().value, word));
       }
     });
@@ -37,53 +47,53 @@ describe('Completer', function () {
 
   //-------------------------------------------------
 
-  describe('findCommonPrefix', function () {
-    it('returns [] for empty array', function () {
-      assert.strictEqual(Completer.findCommonPrefix([]), '');
-      assert.strictEqual(Completer.findCommonPrefix(),   '');
+  describe("findCommonPrefix", () => {
+    it("returns [] for empty array", () => {
+      assert.strictEqual(Completer.findCommonPrefix([]), "");
+      assert.strictEqual(Completer.findCommonPrefix(), "");
     });
 
-    it('returns first if array only contains one value', function () {
-      assert.strictEqual(Completer.findCommonPrefix(['xxx']), 'xxx');
+    it("returns first if array only contains one value", () => {
+      assert.strictEqual(Completer.findCommonPrefix(["xxx"]), "xxx");
     });
 
-    it('returns common prefix if found', function () {
-      assert.strictEqual(Completer.findCommonPrefix(['amazon', 'ambulance', 'amish']), 'am');
+    it("returns common prefix if found", () => {
+      assert.strictEqual(Completer.findCommonPrefix(["amazon", "ambulance", "amish"]), "am");
     });
 
-    it('returns common prefix if found, all same', function () {
-      assert.strictEqual(Completer.findCommonPrefix(['amazon', 'amazon', 'amazon']), 'amazon');
-      assert.strictEqual(Completer.findCommonPrefix(['l', 'l', 'l']), 'l');
+    it("returns common prefix if found, all same", () => {
+      assert.strictEqual(Completer.findCommonPrefix(["amazon", "amazon", "amazon"]), "amazon");
+      assert.strictEqual(Completer.findCommonPrefix(["l", "l", "l"]), "l");
     });
 
-    it('does not return common prefix if none found', function () {
-      assert.strictEqual(Completer.findCommonPrefix(['dog', 'cat', 'cow']), '');
-      assert.strictEqual(Completer.findCommonPrefix(['cat', 'cow', 'dog']), '');
-      assert.strictEqual(Completer.findCommonPrefix(['cat', '', 'cat']), '');
+    it("does not return common prefix if none found", () => {
+      assert.strictEqual(Completer.findCommonPrefix(["dog", "cat", "cow"]), "");
+      assert.strictEqual(Completer.findCommonPrefix(["cat", "cow", "dog"]), "");
+      assert.strictEqual(Completer.findCommonPrefix(["cat", "", "cat"]), "");
     });
   });
 
   //-------------------------------------------------
 
-  describe('findCompletions', function () {
-    it('returns commonPrefix and matches', function () {
-      assert.deepEqual(Completer.findCompletions('am', ['amazon', 'ambulance', 'amish']), ['am', 'amazon', 'ambulance', 'amish']);
-      assert.deepEqual(Completer.findCompletions('a',  ['amazon', 'ambulance', 'amish']), ['am', 'amazon', 'ambulance', 'amish']);
-      assert.deepEqual(Completer.findCompletions('a',  ['antidote', 'antigravity', 'spaceship']), ['anti', 'antidote', 'antigravity']);
+  describe("findCompletions", () => {
+    it("returns commonPrefix and matches", () => {
+      assert.deepEqual(Completer.findCompletions("am", ["amazon", "ambulance", "amish"]), ["am", "amazon", "ambulance", "amish"]);
+      assert.deepEqual(Completer.findCompletions("a", ["amazon", "ambulance", "amish"]), ["am", "amazon", "ambulance", "amish"]);
+      assert.deepEqual(Completer.findCompletions("a", ["antidote", "antigravity", "spaceship"]), ["anti", "antidote", "antigravity"]);
     });
 
-    it('returns itself if no matches', function () {
-      assert.deepEqual(Completer.findCompletions('a',     ['dog', 'cat', 'cow']), ['a']);
-      assert.deepEqual(Completer.findCompletions('anti',  ['dog', 'cat', 'cow']), ['anti']);
+    it("returns itself if no matches", () => {
+      assert.deepEqual(Completer.findCompletions("a", ["dog", "cat", "cow"]), ["a"]);
+      assert.deepEqual(Completer.findCompletions("anti", ["dog", "cat", "cow"]), ["anti"]);
     });
 
-    it('does not return duplicates if in matches', function () {
-      assert.deepEqual(Completer.findCompletions('cat', ['catalog', 'cat', 'cathode']), ['cat', 'catalog', 'cathode']);
-      assert.deepEqual(Completer.findCompletions('cat', ['cat']), ['cat']);
+    it("does not return duplicates if in matches", () => {
+      assert.deepEqual(Completer.findCompletions("cat", ["catalog", "cat", "cathode"]), ["cat", "catalog", "cathode"]);
+      assert.deepEqual(Completer.findCompletions("cat", ["cat"]), ["cat"]);
     });
 
-    it('does not return duplicates if same as commonPrefix', function () {
-      assert.deepEqual(Completer.findCompletions('cat', ['cathode']), ['cathode']);
+    it("does not return duplicates if same as commonPrefix", () => {
+      assert.deepEqual(Completer.findCompletions("cat", ["cathode"]), ["cathode"]);
     });
   });
 });
