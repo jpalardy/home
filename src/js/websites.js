8e61743057a015a0e410579841d34a3d3729648f
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-03-31 14:26:26 -0700

    Add canadian physical shopping

--- src/js/websites.js
+++ src/js/websites.js
@@ -247,6 +247,18 @@ module.exports = [
     alias: "devhints",
     search: "https://devhints.io/?q=%s",
   },
+  {
+    alias: "ct",
+    search: "https://www.canadiantire.ca/en/search-results.html?q=%s",
+  },
+  {
+    alias: "bb",
+    search: "https://www.bestbuy.ca/en-CA/Search/SearchResults.aspx?query=%s",
+  },
+  {
+    alias: "hd",
+    search: "https://www.homedepot.ca/en/home/search.html?q=%s",
+  },
 ].map(site => {
   return {
     alias: site.alias,
