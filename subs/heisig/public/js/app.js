8f697ec8929eb2880c8ee37f8ca31d4923098ab9
Jonathan Palardy <jonathan.palardy@gmail.com>
2019-05-17 20:43:37 -0700

    Update JavaScript to es6+

--- subs/heisig/public/js/app.js
+++ subs/heisig/public/js/app.js
@@ -1,8 +1,8 @@
 /* global fetch, Vue, document, location, starific */
 
-var app = new Vue({
-  el: '#app',
-  template: '#template',
+const app = new Vue({
+  el: "#app",
+  template: "#template",
   data: {
     query: "",
     allCards: [],
@@ -10,16 +10,18 @@ var app = new Vue({
   //-------------------------------------------------
   computed: {
     count() {
-      if (this.cards.length === 0) { return "no kanjis"; }
-      return (this.cards.length === 1 ? "1 kanji" : `${this.cards.length} kanjis`);
+      if (this.cards.length === 0) {
+        return "no kanjis";
+      }
+      return this.cards.length === 1 ? "1 kanji" : `${this.cards.length} kanjis`;
     },
     cards() {
-      let query = this.query;
+      let {query} = this;
       if (!query.match(/[ *]$/)) {
-        query += '*';
+        query += "*";
       }
       const matchers = starific(query);
-      const cards    = this.allCards;
+      const cards = this.allCards;
       if (!query) {
         return cards;
       }
@@ -29,40 +31,38 @@ var app = new Vue({
   //-------------------------------------------------
   watch: {
     query() {
-      location.hash = encodeURIComponent(this.query).replace(/%20/g, '+');
-      document.title = (this.query.trim() ? `Heisig: ${this.query}` : 'Heisig lookup');
+      location.hash = encodeURIComponent(this.query).replace(/%20/g, "+");
+      document.title = this.query.trim() ? `Heisig: ${this.query}` : "Heisig lookup";
     },
   },
   //-------------------------------------------------
   created() {
     // pick query from the URL
-    this.query = decodeURIComponent(location.hash.replace(/^#/, '').replace(/\+/g, "%20"));
+    this.query = decodeURIComponent(location.hash.replace(/^#/, "").replace(/\+/g, "%20"));
     // load JSON
-    fetch('heisig.min.json').then(response => response.json()).then((cards) => {
-      const tokenize = function (str) {
-        // "muffin's" -> "muffin's muffin"
-        const possessiveWord = function (word) {
-          return [word, word.replace(/'s\b/, '')].join(' ');
+    fetch("heisig.min.json")
+      .then(response => response.json())
+      .then(cards => {
+        const tokenize = str => {
+          // "muffin's" -> "muffin's muffin"
+          const possessiveWord = word => [word, word.replace(/'s\b/, "")].join(" ");
+          return str
+            .toLowerCase()
+            .replace(/-/g, " ")
+            .replace(/\w+'s\b/, possessiveWord)
+            .replace(/[^a-z' ]/g, "")
+            .trim()
+            .split(/\s+/);
         };
-        return str.toLowerCase()
-                  .replace(/-/g, ' ')
-                  .replace(/\w+'s\b/, possessiveWord)
-                  .replace(/[^a-z' ]/g, '')
-                  .trim()
-                  .split(/\s+/);
-      };
-      this.allCards = cards.map((card, i) => {
-        const result = {};
-        result.no      = `${i + 1}`;
-        result.key     = `card-${result.no}`;
-        result.kanji   = Object.keys(card)[0];
-        result.keyword = card[result.kanji];
-        result.tokens  = [...tokenize(result.keyword), result.no, result.kanji];
-        return result;
-      });
-    }).catch((err) => {
-      console.error(err);
-    });
+        this.allCards = cards.map((card, i) => {
+          const result = {};
+          result.no = `${i + 1}`;
+          result.key = `card-${result.no}`;
+          [result.kanji, result.keyword] = Object.entries(card).flat();
+          result.tokens = [...tokenize(result.keyword), result.no, result.kanji];
+          return result;
+        });
+      })
+      .catch(err => console.error(err));
   },
 });
-
