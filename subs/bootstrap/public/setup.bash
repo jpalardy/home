979bdaf5533fa12b5701c98a88d0e71e6016f22e
Jonathan Palardy <jonathan.palardy@gmail.com>
2020-08-22 16:37:39 -0700

    Add ignored bootstrap

---
+++ subs/bootstrap/public/setup.bash
@@ -0,0 +1,6 @@
+
+curl https://home.jpalardy.com/bootstrap/vimrc -o "$HOME/.vimrc"
+curl https://home.jpalardy.com/bootstrap/tmux.conf -o "$HOME/.tmux.conf"
+
+echo "alias ll='ls -l'" >> "$HOME/.bashrc"
+
