a99d1a4aad84b7fd8462187cb8c4194b1472339a
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-08-14 12:02:54 -0700

    Clean, re-organize docs aliases

--- src/js/sites/docs.js
+++ src/js/sites/docs.js
@@ -3,6 +3,9 @@ function capitalize(str) {
 }
 
 module.exports = [
+  // -------------------------------------------------
+  // misc
+  // -------------------------------------------------
   {
     alias: "jq",
     visit: "https://stedolan.github.io/jq/manual/",
@@ -13,36 +16,20 @@ module.exports = [
     visit: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference",
   },
   {
-    alias: "golang",
-    search: "https://pkg.go.dev/search?q=%s",
+    alias: "postgres",
+    search: "https://www.postgresql.org/search/?q=%s",
   },
-  {
-    alias: "rust",
-    search: "https://doc.rust-lang.org/std/index.html?search=%s",
-    visit: "https://doc.rust-lang.org/std/index.html",
-  },
+  // -------------------------------------------------
+  // elixir, phoenix
+  // -------------------------------------------------
   {
     alias: "hex",
     search: "https://hex.pm/packages?search=%s",
   },
   {
-    alias: "hd",
-    search: "https://hexdocs.pm/%s/",
-  },
-  {
     alias: "exm",
     search: "https://hexdocs.pm/elixir/%s.html#summary",
     queryMod: capitalize,
-  },
-  {
-    alias: "ex",
-    search: "https://hexdocs.pm/elixir/search.html?q=%s",
-    visit: "https://elixir-lang.org/docs.html",
-  },
-  {
-    alias: "phx",
-    search: "https://hexdocs.pm/phoenix/search.html?q=%s",
-    visit: "https://www.phoenixframework.org/",
   },
   {
     alias: "ef",
@@ -63,10 +50,9 @@ module.exports = [
     visit: "https://www.google.com/search?q=elixir+phoenix",
     search: "https://www.google.com/search?q=elixir+phoenix+%s",
   },
-  {
-    alias: "postgres",
-    search: "https://www.postgresql.org/search/?q=%s",
-  },
+  // -------------------------------------------------
+  // elm
+  // -------------------------------------------------
   {
     alias: "gelm",
     visit: "https://www.google.com/search?q=elm",
