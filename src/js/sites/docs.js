0b392df9f67c786e721c1b4dddc0ca668ccc274d
Jonathan Palardy <jonathan.palardy@gmail.com>
2021-12-10 11:16:15 -0500

    Add exm@module aliases

--- src/js/sites/docs.js
+++ src/js/sites/docs.js
@@ -56,8 +56,13 @@ module.exports = [
   {
     alias: "exm",
     search: "https://hexdocs.pm/elixir/%s.html#summary",
+    visit: "https://hexdocs.pm/elixir/",
     queryMod: capitalize,
   },
+  ["Kernel", "String", "Enum", "Keyword", "List", "Map"].map((name) => ({
+    alias: `exm@${name.toLowerCase()}`,
+    visit: `https://hexdocs.pm/elixir/${name}.html#summary`,
+  })),
   {
     alias: "ef",
     search: "https://elixirforum.com/search?q=%s",
