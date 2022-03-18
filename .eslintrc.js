a58bc3ca07101ba221ff216078f28b64c948a4fd
Jonathan Palardy <jonathan.palardy@gmail.com>
2022-03-18 15:19:11 -0700

    Update eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// http://eslint.org/docs/rules/
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended"],
+};
