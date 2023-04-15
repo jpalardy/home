468bd25dbe8cac08c6e7ce3d91a424448ba65264
Jonathan Palardy <jonathan.palardy@gmail.com>
2023-04-15 12:49:44 -0700

    Add nebula alias

--- subs/home/src/ts/sites/websites.ts
+++ subs/home/src/ts/sites/websites.ts
@@ -223,4 +223,9 @@ export const sites: SiteConfig[] = [
     alias: "chatgpt",
     visit: "https://chat.openai.com/chat",
   },
+  {
+    alias: "nebula",
+    search: "https://nebula.tv/search?q=%s",
+    visit: "https://nebula.tv/myshows",
+  },
 ];
