577a92ddccbae4af5395255ae60b405f0faedba0
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-05-21 09:50:02 -0700

    Add "shift" to decoded keys

--- subs/home/src/KeyDecoder.elm
+++ subs/home/src/KeyDecoder.elm
@@ -12,9 +12,9 @@ type alias CustomMsg a =
 
 combiner : (String -> msg) -> Json.Decode.Decoder msg
 combiner msg =
-    Json.Decode.map2
-        (\key ctrlKey ->
-            [ ( "Control", ctrlKey ), ( key, True ) ]
+    Json.Decode.map3
+        (\key ctrlKey shiftKey ->
+            [ ( "Control", ctrlKey ), ( "Shift", shiftKey ), ( key, True ) ]
                 |> List.filter Tuple.second
                 |> List.map Tuple.first
                 |> String.join "-"
@@ -22,6 +22,7 @@ combiner msg =
         )
         (Json.Decode.field "key" Json.Decode.string)
         (Json.Decode.field "ctrlKey" Json.Decode.bool)
+        (Json.Decode.field "shiftKey" Json.Decode.bool)
 
 
 tabCatcher : msg -> Json.Decode.Decoder (CustomMsg msg)
