36506b90ac8c1d35585a645ab2080488c9c35da3
Jonathan Palardy <jonathan.palardy@gmail.com>
2025-12-31 16:43:44 -0800

    Fix meta-handling for single-character input

--- subs/home/src/KeyDecoder.elm
+++ subs/home/src/KeyDecoder.elm
@@ -12,17 +12,30 @@ type alias CustomMsg a =
 
 combiner : (String -> msg) -> Json.Decode.Decoder msg
 combiner msg =
-    Json.Decode.map3
-        (\key ctrlKey shiftKey ->
-            [ ( "Control", ctrlKey ), ( "Shift", shiftKey ), ( key, True ) ]
-                |> List.filter Tuple.second
-                |> List.map Tuple.first
+    let
+        decodeMaybeKey : String -> Json.Decode.Decoder (Maybe String)
+        decodeMaybeKey keyName =
+            Json.Decode.bool
+                |> Json.Decode.map
+                    (\bool ->
+                        if bool then
+                            Just keyName
+
+                        else
+                            Nothing
+                    )
+    in
+    Json.Decode.map4
+        (\metaKey ctrlKey shiftKey key ->
+            [ metaKey, ctrlKey, shiftKey, Just key ]
+                |> List.filterMap identity
                 |> String.join "-"
                 |> msg
         )
+        (Json.Decode.field "metaKey" <| decodeMaybeKey "Meta")
+        (Json.Decode.field "ctrlKey" <| decodeMaybeKey "Ctrl")
+        (Json.Decode.field "shiftKey" <| decodeMaybeKey "Shift")
         (Json.Decode.field "key" Json.Decode.string)
-        (Json.Decode.field "ctrlKey" Json.Decode.bool)
-        (Json.Decode.field "shiftKey" Json.Decode.bool)
 
 
 tabCatcher : msg -> Json.Decode.Decoder (CustomMsg msg)
